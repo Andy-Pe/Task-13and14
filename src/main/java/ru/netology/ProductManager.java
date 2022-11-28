71f466ff07150e0a1bfc032e3bcfa4abf21d072d
Andrey Petrov <Pandand1@yandex.ru>
2022-11-28 12:12:01 +0300

    Change SearchBy and add tests

--- src/main/java/ru/netology/ProductManager.java
+++ src/main/java/ru/netology/ProductManager.java
@@ -21,6 +21,21 @@ public class ProductManager {
         return reversed;
     }
 
+    public Product[] searchBy(String text) {
+        Product[] result = new Product[0];
+        for (Product product : repo.getProduct()) {
+            if (matches(product, text)) {
+                Product[] tmp = new Product[result.length + 1];
+                for (int i = 0; i < result.length; i++) {
+                    tmp[i] = result[i];
+                }
+                tmp[result.length] = product;
+                result = tmp;
+            }
+        }
+        return result;
+    }
+
     // метод определения соответствия товара product запросу search
     public boolean matches(Product product, String search) {
         if (product.getName().contains(search)) {
@@ -28,15 +43,5 @@ public class ProductManager {
         } else {
             return false;
         }
-    }
-
-    public Product[] searchBy(String text) {
-        Product[] result = new Product[0];
-        for (Product product : repo.getProduct()) {
-            if (matches(product, text)) {
-                result = repo.getProduct();
-            }
-        }
-        return result;
     }
 }
--- src/test/java/ru/netology/ProductManagerTest.java
+++ src/test/java/ru/netology/ProductManagerTest.java
@@ -11,10 +11,14 @@ class ProductManagerTest {
     ProductManager manager = new ProductManager(repo);
 
     Product good1 = new Product(5, "Green Mile", 200);
-    Product good2 = new Product(74, "Iphone", 1200);
+    Product good2 = new Product(74, "Iphone5", 1200);
     Product good3 = new Product(87, "John's Adventures", 300);
     Product good4 = new Product(16, "Xiaomi", 800);
     Product good5 = new Product(25, "Nokia", 400);
+    Product good6 = new Product(41, "Iphone14", 1200);
+    Product good7 = new Product(4, "Iphone11", 1200);
+    Product good8 = new Product(7, "John's secrets", 1200);
+
 
     @BeforeEach
     public void setup() {
@@ -23,22 +27,52 @@ class ProductManagerTest {
         manager.add(good3);
         manager.add(good4);
         manager.add(good5);
+        manager.add(good6);
+        manager.add(good7);
+        manager.add(good8);
     }
 
     @Test
     public void shouldReverseAllGoods() {
 
-        Product[] expected = {good5, good4, good3, good2, good1};
+        Product[] expected = {good8, good7, good6, good5, good4, good3, good2, good1};
         Product[] actual = manager.getGoods();
 
         Assertions.assertArrayEquals(expected, actual);
     }
 
     @Test
-    public  void shouldSearchByText() {
+    public void shouldSearchByTextWithIphone() {
 
-        Product[] expected = {good1, good2, good3, good4, good5};
+        Product[] expected = {good2, good6, good7};
         Product[] actual = manager.searchBy("Iphone");
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSearchByTextWithJohn() {
+
+        Product[] expected = {good3, good8};
+        Product[] actual = manager.searchBy("John");
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindNothingCoincidence() {
+
+        Product[] expected = {};
+        Product[] actual = manager.searchBy("Doctor Strength");
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindOnlyOneCoincidence() {
+
+        Product[] expected = {good1};
+        Product[] actual = manager.searchBy("Green Mile");
 
         Assertions.assertArrayEquals(expected, actual);
     }
